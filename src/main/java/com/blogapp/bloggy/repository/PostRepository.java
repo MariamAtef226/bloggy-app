24d7933c9608c880ad153d5ec78ed35dd0c6bd47
mariamatef226 <mariamatef226@gmail.com>
2024-03-10 16:18:51 +0200

    finished course

--- src/main/java/com/blogapp/bloggy/repository/PostRepository.java
+++ src/main/java/com/blogapp/bloggy/repository/PostRepository.java
@@ -6,13 +6,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 //@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post>  findByCategoryId(Long categoryId);
+    List<Post> findByCategoryId(Long categoryId);
 
     // JPQL queries
 //    @Query("SELECT p from Post p WHERE " +
@@ -26,4 +27,12 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             "or p.description LIKE CONCAT('%',:query,'%') " +
             "or p.content LIKE CONCAT('%',:query,'%')", nativeQuery = true)
     Page<Post> searchPosts(String query, Pageable pageable);
+
+    @Override
+    @Transactional(timeout = 10)
+    void deleteById(Long Id);
+
+    @Override
+    @Transactional(propagation = Propagation.NEVER)
+    <S extends Post> S save(S entity);
 }
